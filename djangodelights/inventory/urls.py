71909c6dd177c3be253d43388de993fba9d05a39
Sungbin_Kang <sungbin9353@gmail.com>
2021-12-06 13:01:29 -0800

    deleted delete templates for models and commented out urls and views

--- djangodelights/inventory/urls.py
+++ djangodelights/inventory/urls.py
@@ -2,33 +2,35 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path("", HomeView.as_view(), name="home"),
     path("", views.home, name="home"),
     path("account/", include("django.contrib.auth.urls")),
     path("account/signup/", views.SignUp.as_view(), name="signup"),
 
     # menu/
     path("menu/", views.MenuItemList.as_view(), name="menuitem_list"),
-    path("menu/new/", views.MenuItemCreateView.as_view(), name="menuitem_add"),
-    path("menu/update/<int:pk>", views.MenuItemUpdateView.as_view(), name="menuitem_update"),
-    path("menu/delete/<int:pk>", views.MenuItemDeleteView.as_view(), name="menuitem_delete"),
+    # path("menu/new/", views.MenuItemCreateView.as_view(), name="menuitem_add"),
+    # path("menu/update/<int:pk>", views.MenuItemUpdateView.as_view(), name="menuitem_update"),
+    # path("menu/delete/<int:pk>", views.MenuItemDeleteView.as_view(), name="menuitem_delete"),
 
     # menu/recipe
-    # change the link to each menu name
     path("menu/<menuitem_title>/recipe/", views.RecipeRequirementList.as_view(), name="recipe_list"),
-    path("menu/<menuitem_title>/recipe/new/", views.RecipeRequirementCreateView.as_view(), name="recipe_add"),
-    path("menu/<menuitem_title>/recipe/update/<int:pk>", views.RecipeRequirementUpdateView.as_view(), name="recipe_update"),
-    path("menu/<menuitem_title>/recipe/delete/<int:pk>", views.RecipeRequirementDeleteView.as_view(), name="recipe_delete"),
+    # path("menu/<menuitem_title>/recipe/new/", views.RecipeRequirementCreateView.as_view(), name="recipe_add"),
+    # path("menu/<menuitem_title>/recipe/update/<int:pk>", views.RecipeRequirementUpdateView.as_view(), name="recipe_update"),
+    # path("menu/<menuitem_title>/recipe/delete/<int:pk>", views.RecipeRequirementDeleteView.as_view(), name="recipe_delete"),
 
     # ingredient/
     path("ingredient/", views.IngredientList.as_view(), name="ingredient_list"),
-    path("ingredient/new/", views.IngredientCreateView.as_view(), name="ingredient_add"),
-    path("ingredient/update/<int:pk>", views.IngredientUpdateView.as_view(), name="ingredient_update"),
-    path("ingredient/delete/<int:pk>", views.IngredientDeleteView.as_view(), name="ingredient_delete"),
+    # path("ingredient/new/", views.IngredientCreateView.as_view(), name="ingredient_add"),
+    # path("ingredient/update/<int:pk>", views.IngredientUpdateView.as_view(), name="ingredient_update"),
+    # path("ingredient/delete/<int:pk>", views.IngredientDeleteView.as_view(), name="ingredient_delete"),
+
+    # purchase/
+    path("purchase/", views.PurchaseList.as_view(), name="report_list"),
+    # path("report/new/", views.PurchaseCreateView.as_view(), name="report_add"),
 
     # report/
-    path("report/", views.PurchaseList.as_view(), name="report_list"),
-    path("report/new/", views.PurchaseCreateView.as_view(), name="report_add"),
+    
+    
 
 
 
--- djangodelights/inventory/views.py
+++ djangodelights/inventory/views.py
@@ -28,21 +28,21 @@ class SignUp(CreateView):
 class MenuItemList(ListView):
     model = MenuItem
 
-class MenuItemCreateView(CreateView):
-    model = MenuItem
-    form_class = MenuItemForm
-    template_name = "inventory/menuitem_add_form.html"
+# class MenuItemCreateView(CreateView):
+#     model = MenuItem
+#     form_class = MenuItemForm
+#     template_name = "inventory/menuitem_add_form.html"
 
-class MenuItemUpdateView(UpdateView):
-    model = MenuItem
-    form_class = MenuItemForm
-    template_name = "inventory/menuitem_update_form.html"
+# class MenuItemUpdateView(UpdateView):
+#     model = MenuItem
+#     form_class = MenuItemForm
+#     template_name = "inventory/menuitem_update_form.html"
 
-class MenuItemDeleteView(DeleteView):
-    model = MenuItem
-    form_class = MenuItemForm
-    template_name = "inventory/menuitem_delete_form.html"
-    success_url = "/menu"
+# class MenuItemDeleteView(DeleteView):
+#     model = MenuItem
+#     form_class = MenuItemForm
+#     template_name = "inventory/menuitem_delete_form.html"
+#     success_url = "/menu"
 
 
 # ----------------------- INGREDENT -------------------------
@@ -50,21 +50,21 @@ class MenuItemDeleteView(DeleteView):
 class IngredientList(ListView):
     model = Ingredient
 
-class IngredientCreateView(CreateView):
-    model = Ingredient
-    form_class = IngredientForm
-    template_name = "inventory/ingredient_add_form.html"
+# class IngredientCreateView(CreateView):
+#     model = Ingredient
+#     form_class = IngredientForm
+#     template_name = "inventory/ingredient_add_form.html"
 
-class IngredientUpdateView(UpdateView):
-    model = Ingredient
-    form_class = IngredientForm
-    template_name = "inventory/ingredient_update_form.html"
+# class IngredientUpdateView(UpdateView):
+#     model = Ingredient
+#     form_class = IngredientForm
+#     template_name = "inventory/ingredient_update_form.html"
 
-class IngredientDeleteView(DeleteView):
-    model = Ingredient
-    form_class = IngredientForm
-    template_name = "inventory/ingredient_delete_form.html"
-    success_url = "/ingredient"
+# class IngredientDeleteView(DeleteView):
+#     model = Ingredient
+#     form_class = IngredientForm
+#     template_name = "inventory/ingredient_delete_form.html"
+#     success_url = "/ingredient"
 
 
 # ------------------- RECIPE REQURIEMENT --------------------
@@ -83,37 +83,37 @@ class RecipeRequirementList(ListView):
         context["menuitem_title"] = self.kwargs["menuitem_title"]
         return context
 
-class RecipeRequirementCreateView(CreateView):
-    model = RecipeRequirement
-    form_class = RecipeRequirementForm
-    template_name = "inventory/recipe_add_form.html"
+# class RecipeRequirementCreateView(CreateView):
+#     model = RecipeRequirement
+#     form_class = RecipeRequirementForm
+#     template_name = "inventory/recipe_add_form.html"
     
-    def get_success_url(self):
-        menuitem_title = self.kwargs["menuitem_title"]
-        return reverse("recipe_list", kwargs={"menuitem_title": menuitem_title})
+#     def get_success_url(self):
+#         menuitem_title = self.kwargs["menuitem_title"]
+#         return reverse("recipe_list", kwargs={"menuitem_title": menuitem_title})
 
-class RecipeRequirementUpdateView(UpdateView):
-    model = RecipeRequirement
-    form_class = RecipeRequirementForm
-    template_name = "inventory/recipe_update_form.html"
+# class RecipeRequirementUpdateView(UpdateView):
+#     model = RecipeRequirement
+#     form_class = RecipeRequirementForm
+#     template_name = "inventory/recipe_update_form.html"
 
-    def get_success_url(self):
-        menuitem_title = self.kwargs["menuitem_title"]
-        return reverse("recipe_list", kwargs={"menuitem_title": menuitem_title})
+#     def get_success_url(self):
+#         menuitem_title = self.kwargs["menuitem_title"]
+#         return reverse("recipe_list", kwargs={"menuitem_title": menuitem_title})
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["menuitem_title"] = self.kwargs["menuitem_title"]
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["menuitem_title"] = self.kwargs["menuitem_title"]
+#         return context
 
-class RecipeRequirementDeleteView(DeleteView):
-    model = RecipeRequirement
-    form_class = RecipeRequirementForm
-    template_name = "inventory/recipe_delete_form.html"
+# class RecipeRequirementDeleteView(DeleteView):
+#     model = RecipeRequirement
+#     form_class = RecipeRequirementForm
+#     template_name = "inventory/recipe_delete_form.html"
     
-    def get_success_url(self):
-        menuitem_title = self.kwargs["menuitem_title"]
-        return reverse("recipe_list", kwargs={"menuitem_title": menuitem_title})
+#     def get_success_url(self):
+#         menuitem_title = self.kwargs["menuitem_title"]
+#         return reverse("recipe_list", kwargs={"menuitem_title": menuitem_title})
 
 
 # ----------------------- PURCHASE --------------------------
@@ -122,48 +122,48 @@ class PurchaseList(ListView):
     model = Purchase
     template_name = "inventory/report_list.html"
 
-class PurchaseCreateView(CreateView):
-    model = Purchase
-    form_class = PurchaseForm
-    template_name = "inventory/report_add_form.html"
+# class PurchaseCreateView(CreateView):
+#     model = Purchase
+#     form_class = PurchaseForm
+#     template_name = "inventory/report_add_form.html"
 
-    # if required ingredient for menuitem not enough,
-    # form input select option grayed out
+#     # if required ingredient for menuitem not enough,
+#     # form input select option grayed out
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        available_menuitems = [X for X in MenuItem.objects.all() if X.available()]
-        context["available_menuitems"] = available_menuitems
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         available_menuitems = [X for X in MenuItem.objects.all() if X.available()]
+#         context["available_menuitems"] = available_menuitems
+#         return context
 
-    def post(self, request, *args, **kwargs):
+#     def post(self, request, *args, **kwargs):
 
-        menu_item_id = request.POST["menu_item"]
-        menu_item = MenuItem.objects.get(pk=menu_item_id)
+#         menu_item_id = request.POST["menu_item"]
+#         menu_item = MenuItem.objects.get(pk=menu_item_id)
         
-        for required_recipe in menu_item.reciperequirement_set.all():
-            ingredient_in_stock = required_recipe.ingredient
-            ingredient_in_stock.quantity -= required_recipe.quantity
-            ingredient_in_stock.save()
+#         for required_recipe in menu_item.reciperequirement_set.all():
+#             ingredient_in_stock = required_recipe.ingredient
+#             ingredient_in_stock.quantity -= required_recipe.quantity
+#             ingredient_in_stock.save()
 
-        return super().post(request, *args, **kwargs)
+#         return super().post(request, *args, **kwargs)
 
-class ReportView(LoginRequiredMixin, TemplateView):
-    template_name = "inventory/reports.html"
+# class ReportView(LoginRequiredMixin, TemplateView):
+#     template_name = "inventory/reports.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["purchases"] = Purchase.objects.all()
-        revenue = Purchase.objects.aggregate(
-            revenue=Sum("menu_item__price"))["revenue"]
-        total_cost = 0
-        for purchase in Purchase.objects.all():
-            for recipe_requirement in purchase.menu_item.reciperequirement_set.all():
-                total_cost += recipe_requirement.ingredient.price_per_unit * \
-                    recipe_requirement.quantity
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["purchases"] = Purchase.objects.all()
+#         revenue = Purchase.objects.aggregate(
+#             revenue=Sum("menu_item__price"))["revenue"]
+#         total_cost = 0
+#         for purchase in Purchase.objects.all():
+#             for recipe_requirement in purchase.menu_item.reciperequirement_set.all():
+#                 total_cost += recipe_requirement.ingredient.price_per_unit * \
+#                     recipe_requirement.quantity
 
-        context["revenue"] = revenue
-        context["total_cost"] = total_cost
-        context["profit"] = revenue - total_cost
+#         context["revenue"] = revenue
+#         context["total_cost"] = total_cost
+#         context["profit"] = revenue - total_cost
 
-        return context
+#         return context
